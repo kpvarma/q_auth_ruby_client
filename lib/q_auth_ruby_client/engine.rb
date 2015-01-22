bfce98098cbbeb825b3b1de83e3fc38610ae5deb
Krishnaprasad Varma <kvarma@qwinixtech.com>
2015-01-22 09:52:17 +0530

    Added Application Helper to railtie initializer

--- lib/q_auth_ruby_client/engine.rb
+++ lib/q_auth_ruby_client/engine.rb
@@ -4,9 +4,11 @@ module QAuthRubyClient
 
     initializer "q_auth_ruby_client.configure_rails_initialization" do |app|
       ActiveSupport.on_load :action_controller do
+        include QAuthRubyClient::ApplicationHelper
         include QAuthRubyClient::ApiHelper
         include QAuthRubyClient::NotificationHelper
         include QAuthRubyClient::SessionsHelper
+        helper QAuthRubyClient::ApplicationHelper
         helper QAuthRubyClient::ApiHelper
         helper QAuthRubyClient::NotificationHelper
         helper QAuthRubyClient::SessionsHelper
