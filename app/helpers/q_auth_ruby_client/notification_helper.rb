c8e22fca231b6c81169c6b0fd8ed069e0cdce99d
Krishnaprasad Varma <kvarma@qwinixtech.com>
2015-02-13 15:34:10 +0530

    Uses Poodle 0.1.0

--- app/helpers/q_auth_ruby_client/notification_helper.rb
+++ app/helpers/q_auth_ruby_client/notification_helper.rb
@@ -1,9 +1,9 @@
 module QAuthRubyClient
   module NotificationHelper
-    def set_notification_messages(heading, alert, not_type)
+    def set_notification_messages(heading, alert, now_type)
       @heading = heading
       @alert = alert
-      set_flash_message("#{@heading}: #{@alert}", not_type) if defined?(flash) && flash
+      set_flash_message("#{@heading}: #{@alert}", now_type) if defined?(flash) && flash
     end
   end
 end
